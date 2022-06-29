ee033ad0bb4ad7eb45bad8f61295155d046030c3
Brian <koimad@live.co.uk>
2022-06-30 00:52:45 +0100

    Update to Version 10

--- src/app/components/controls/component-error-mapper.ts
+++ src/app/components/controls/component-error-mapper.ts
@@ -1,6 +1,8 @@
 import { FormControl, FormGroupDirective, NgForm } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
+import { Injectable } from "@angular/core";
 
+@Injectable()
 export class ComponentErrorMapper implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
     return control && control.invalid;
