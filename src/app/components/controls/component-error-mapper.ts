1ec7872cf2ddf9345b10cb3b0c6f703389a90c82
Brian <koimad@live.co.uk>
2022-06-30 00:34:12 +0100

    Revert "Update to angular 10" This reverts commit a94a907f2d3d408d5078849078a61c032ad23df4.

--- src/app/components/controls/component-error-mapper.ts
+++ src/app/components/controls/component-error-mapper.ts
@@ -1,8 +1,6 @@
 import { FormControl, FormGroupDirective, NgForm } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
-import { Injectable } from "@angular/core";
 
-@Injectable()
 export class ComponentErrorMapper implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
     return control && control.invalid;
